ea1cf7c988d372cea4515506caf7e94b6dc50d28
rlokesh07 <63570802+rlokesh07@users.noreply.github.com>
2024-05-10 00:07:55 -0400

    Snap to equation done

--- src/routes/modules/Applications of Derivatives/+page.svelte
+++ src/routes/modules/Applications of Derivatives/+page.svelte
@@ -13,21 +13,30 @@
 	function handlePositionChange1(event) {
 		position[0].x = event.detail.left;
 		position[0].y = event.detail.top;
+		console.log(position[0].x);
+		console.log(position[0].y);
 		const newD = `M ${10+ position[0].x} ${10 + position[0].y} C 40 10, 65 10, ${95 - position[0].x} ${10 + position[0].y}`;
-		console.log(draggableTop);
-		console.log(draggableLeft);
-		console.log(newD);
 		document.getElementById('1').setAttribute('d', newD)
 	}
 	function handlePositionChange2(event) {
 		position[1].x = event.detail.left;
 		position[1].y = event.detail.top;
-		const newD = `M ${10+ position[1].x} ${10 + position[1].y} C 40 110, 65 110, ${95 - position[1].x} ${10 + position[1].y}`;
+		const desiredPosition = { x: 95, y: 72 };
+
+		var newD;
+		var distance = Math.sqrt(Math.pow(position[1].x -95, 2) + Math.pow(position[1].y -72, 2))
+		if (distance < 8){
+			 newD = `M ${0 - position[1].x + desiredPosition.x} ${80 - position[1].y + desiredPosition.y} C ${55 - position[1].x + desiredPosition.x} ${220 - position[1].y + desiredPosition.y}, ${85 - position[1].x + desiredPosition.x} ${80 - position[1].y + desiredPosition.y}, ${105 - position[1].x + desiredPosition.x} ${0 - position[1].y + desiredPosition.y}`;
+			 console.log(newD)
+		} else{
+			 newD = `M${0 - position[1].x + desiredPosition.x} ${80 - position[1].y + desiredPosition.y} C 55 220, 85 80, ${105 - position[1].x + desiredPosition.x} ${0 - position[1].y + desiredPosition.y}`;
+		}
+
 		console.log(position[1].x);
 		console.log(position[1].y);
-		console.log(newD);
+		console.log(distance);
 		document.getElementById('2').setAttribute('d', newD)
-	}0
+	}
 
 	onMount(() => {
 		var draw = SVG().addTo('Draggable').size(100, 100)
@@ -38,7 +47,7 @@
 
 </script>
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="300" height="300">
-	<path id="graph" d="M 10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80" stroke="blue" stroke-width="4" fill="none"/>
+	<path id="graph" d="M 10 80 C 40 10, 65 10, 95 80 C 150 220, 180 80, 200 0" stroke="blue" stroke-width="4" fill="none"/>
 </svg>
 <Draggable bind:left={position[0].x} bind:top={position[0].y} on:positionChange={handlePositionChange1}>
 		<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100">
@@ -46,8 +55,8 @@
 		</svg>
 </Draggable>
 <Draggable bind:left={position[1].x} bind:top={position[1].y} on:positionChange={handlePositionChange2}>
-	<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100">
-		<path id="2" d="M 10 40 C 40 110, 65 110, 95 40" stroke="blue" stroke-width="4" fill="none"/>
+	<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="200">
+		<path id="2" d="M0 80 C 55 220, 85 80, 105 0" stroke="blue" stroke-width="4" fill="none"/>
 	</svg>
 </Draggable>
 
