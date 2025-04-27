942b545171fe459dd4164dce17a3b926040aa156
rlokesh07 <63570802+rlokesh07@users.noreply.github.com>
2025-04-27 17:48:36 -0400

    added magnetism

--- src/routes/modules/Magnetism/+page.svelte
+++ src/routes/modules/Magnetism/+page.svelte
@@ -21,7 +21,7 @@
 			<p>The direction of the field is determined using the cross product formula. You can also use the right hand rule</p>
 			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9DWA1VG09TOVYMAJx3zHNpjPO1tZxbZmVBQ&s">
 
-			<br>
+
 
 			<p>For a straight wire carrying current I, the force in a magnetic field is</p>
 			<p>{@html renderTextWithMathMode('$$F = IlB \\sin(\\theta)$$')}</p>
@@ -31,8 +31,26 @@
 				<p>When a proton enters a constant magnetic field, the force always points perpendicular to the velocity, causing a centripetal net force</p>
 				<img src="https://openstax.org/apps/archive/20250226.165223/resources/39801086ff242e6af70fdc752a02d98f48b37ae2">
 			</div>
+			<h3>Magnetic Flux</h3>
+			<div>
+				<p>{@html renderTextWithMathMode('Magnetic flux describes the amount of magnetic field passing through a given area. It is represented by the symbol $$\\Phi_B$$ and is calculated using the formula:')}</p>
+				<p>{@html renderTextWithMathMode('$$\\Phi_B = B A \\cos(\\theta)$$')}</p>
+				<p>{@html renderTextWithMathMode('<strong>$$B$$</strong> is the magnetic field strength (measured in Tesla)')}</p>
+				<p>{@html renderTextWithMathMode('<p><strong>$$A$$</strong> is the area through which the magnetic field lines pass (measured in square meters)</p>\n')}</p>
+				<p>{@html renderTextWithMathMode('<p><strong>$$\\theta$$</strong> is the angle between the magnetic field lines and the normal (perpendicular) to the area</p>\n')}</p>
+
+				<p>{@html renderTextWithMathMode('If the magnetic field lines are perpendicular to the surface, $$\\theta$$ is 0° and the flux is maximized. If the field lines are parallel to the surface, $$\\theta$$ is 90° and the flux is zero.')}</p>
+			</div>
+
 		</div>
 		<h2>Changing Magnetic Fields</h2>
+		<div>
+			<p>Just like how you can create magnetic fields with current, you can generate a current through a magnetic field, which is called induction</p>
+			<p>Because there is a current generated, there is a emf generated due to the change of flux</p>
+			<h3>{@html renderTextWithMathMode('$$\\epsilon = -N \\frac{\\Delta \\Phi_B}{\\Delta t}$$')}</h3>
+			<p>{@html renderTextWithMathMode('Where $$\\Delta \\Phi_B$$ is the change in flux and $$\\Delta t$$ is the change in time')}</p>
+		</div>
+
 	</div>
 
 
@@ -43,7 +61,7 @@
 </div>
 <style>
     div {
-        padding-bottom: 10px;
+        padding-bottom: 40px;
         padding-left: 40px;
     }
     p {
