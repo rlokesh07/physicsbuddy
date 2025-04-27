8ad77214ca736b5e3a8339b524bd636b4c49f215
rlokesh07 <63570802+rlokesh07@users.noreply.github.com>
2025-04-27 17:14:16 -0400

    added circuits

---
+++ src/routes/modules/Circuits/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { renderTextWithMathMode } from '$lib/KaTeX';
+</script>
+<div>
+
+	<h1>Circuits</h1>
+	<div>
+		<h2>Current</h2>
+		<div>
+			<p>Electrons like to move from places of high electric potential to place of low electric potential</p>
+			<p>In a battery, this is going from the negative terminal to the positive terminal. </p>
+			<p>{@html renderTextWithMathMode('Current is defined as $$\\frac{\\Delta Q}{\\Delta t}$$ where Q is the charge that flows over an area, and t is the time it flows by')}</p>
+			<p>{@html renderTextWithMathMode('The unit for current is amp, which is defined as one colomb per second')}</p>
+			<div>
+				<h3>Direct Current</h3>
+				<p>Direct current is when the current just goes in one direction</p>
+				<h3>Alternating Current</h3>
+				<p>The current switches forwards and backwards</p>
+			</div>
+		</div>
+		<h2>Ohm's Law</h2>
+		<div>
+			<h4>{@html renderTextWithMathMode('V = IR')}</h4>
+			<p>Where V is voltage, I is current, and R is resistance in ohms</p>
+			<p>Something is considered <em>ohmic</em> if it follows ohms law</p>
+		</div>
+
+		<h2>Circuits</h2>
+		<div>
+			<h3>Series</h3>
+			<div>
+				<p>The resistance of all the resistors adds up. To calculate the current of the circuit, add up all the resistance and plug that into ohms law</p>
+			</div>
+			<h3>Parallel</h3>
+			<div>
+				<p>The resistance in a circuit in parallel is </p>
+				<p>{@html renderTextWithMathMode('$$R = \\frac{I}{\\frac{1}{R_1} + \\frac{1}{R_2} ... + \\frac{1}{R_n}}$$')}</p>
+			</div>
+		</div>
+		<h2>Power</h2>
+		<div>
+			<p>{@html renderTextWithMathMode('Electrical power is given in watts which is a SI unit for joules per second')}</p>
+			<h3>{@html renderTextWithMathMode('$$P = IV = \\frac{V^2}{R}$$')}</h3>
+		</div>
+	</div>
+
+
+
+</div>
+<style>
+    div {
+        padding-bottom: 10px;
+        padding-left: 40px;
+    }
+    p {
+        font-size: 20px;
+    }
+    img {
+        height: 50vh;
+    }
+</style>
---
+++ src/routes/modules/Magnetism/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { renderTextWithMathMode } from '$lib/KaTeX';
+</script>
+<div>
+
+	<h1>Magnetism</h1>
+	<div>
+		<h2>Magnetic Fields</h2>
+		<div>
+			<p>A magnet creates a magnetic field around it that describes the force exerted on other magnets placed in the field.</p>
+			<p>When a wire has a current going through it, you can use the right hand rule to determine the direction of the magnetic field</p>
+			<img src="https://openstax.org/apps/archive/20250226.165223/resources/943acc26c844edd99d683834e8f592a563ab7573">
+			<p>When you create coils of wire with current going through it, it acts like an electromagnet</p>
+			<img src="https://openstax.org/apps/archive/20250226.165223/resources/85edb53f7d1d77733ae30a205ac4ec11cb4504cb">
+
+		</div>
+		<h2>Magnetic Force</h2>
+		<div>
+			<p>{@html renderTextWithMathMode('For a charge $$q$$ moving with a velocity $$\\vec{v}$$ through a field $$\\vec{B}$$, the force $$F$$ is')}</p>
+			<p>{@html renderTextWithMathMode('$$F = qvB \\sin(\\theta)$$')}</p>
+			<p>The direction of the field is determined using the cross product formula. You can also use the right hand rule</p>
+			<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9DWA1VG09TOVYMAJx3zHNpjPO1tZxbZmVBQ&s">
+
+			<br>
+
+			<p>For a straight wire carrying current I, the force in a magnetic field is</p>
+			<p>{@html renderTextWithMathMode('$$F = IlB \\sin(\\theta)$$')}</p>
+			<p>Where l is the length of the wire</p>
+			<h3>Trajectory of a Charge in a Magnetic Field</h3>
+			<div>
+				<p>When a proton enters a constant magnetic field, the force always points perpendicular to the velocity, causing a centripetal net force</p>
+				<img src="https://openstax.org/apps/archive/20250226.165223/resources/39801086ff242e6af70fdc752a02d98f48b37ae2">
+			</div>
+		</div>
+		<h2>Changing Magnetic Fields</h2>
+	</div>
+
+
+
+
+
+
+</div>
+<style>
+    div {
+        padding-bottom: 10px;
+        padding-left: 40px;
+    }
+    p {
+        font-size: 20px;
+    }
+    img {
+        height: 50vh;
+    }
+</style>
